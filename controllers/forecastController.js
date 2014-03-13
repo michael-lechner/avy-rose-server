c171c8daba5f7ad997c5267bf1bf90bbb59587d2
michael lechner <mlechner2@gmail.com>
2014-03-13 10:58:10 -0600

    Upgrades

--- controllers/forecastController.js
+++ controllers/forecastController.js
@@ -1,14 +1,6 @@
 var ForecastModel = require('../models/forecastModel');
 var mongoose = require('mongoose');
 
-var compare = function (a,b) {
-  if (a.scrapeDate < b.scrapeDate)
-     return 1;
-  if (a.scrapeDate > b.scrapeDate)
-    return -1;
-  return 0;
-}
-
 var forecastController = module.exports = {
     find: function (req, res) {
         ForecastModel.find(function (err, forecasts) {
@@ -19,11 +11,9 @@ var forecastController = module.exports = {
         });
     },
     findAll: function (req, res) {
-        ForecastModel.find(function (err, forecasts) {
+        ForecastModel.find().sort('-scrapeDate').exec(function (err, forecasts) {
             if(err) console.log(err);
-
-            forecasts.sort(compare);
             res.send(forecasts.slice(0));
-        });        
+        });
     }
 }
--- scraper.js
+++ scraper.js
@@ -12,28 +12,6 @@ var lvlLookup = {
     Alp: 'at'
 }
 
-var cleanHouse = function (mostRecent) {
-    ForecastModel.find(function (err, forecasts) {
-        if(err) console.log(err);
-
-        forecasts.sort(compare);
-
-        for(var i = (mostRecent - 1); i < forecasts.length; i++){
-            ForecastModel.findByIdAndRemove(forecasts[i]._id, function (err, data) {
-                if(err) console.log(err);
-            });
-        }
-    })
-}
-
-var compare = function (a,b) {
-  if (a.scrapeDate < b.scrapeDate)
-     return 1;
-  if (a.scrapeDate > b.scrapeDate)
-    return -1;
-  return 0;
-}
-
 var forecastBuilder = function (el) {
     var forecast = {};
     var idStr, dir, lvl = '';
@@ -57,57 +35,68 @@ var forecastBuilder = function (el) {
     return forecast;
 }
 
+var getReport = function () {
+    request('http://avalanche.state.co.us/caic/pub_bc_avo.php?zone_id=1', function (error, response, body) {
+
+        if(error) console.log(error);
+
+        if (!error && response.statusCode == 200) {
+            $ = cheerio.load(body);
+
+            var forecast = [];
+
+            var report = new ForecastModel();
+
+            /* forecast date */
+            var forecastDate = $('.caption-controls').first().html();
+            forecastDate = forecastDate.substring(0, forecastDate.indexOf('<span'));
+            report.forecastDate = forecastDate.replace(/(\t|\n)+/g, "").trim();
+
+            /* zone 
+             * hard coded because requires a different url 
+            */
+            var zone = 'front range'
+            report.zone = zone;
+
+            /* danger ratings */
+            var at = $('.above-treeline-image').closest('tr').find('.today-text strong').text();
+            report.atRating = ratingBuilder(at);
+            var tl = $('.near-treeline-image').closest('tr').find('.today-text strong').text();
+            report.tlRating = ratingBuilder(tl);
+            var bt = $('.below-treeline-image').closest('tr').find('.today-text strong').text();
+            report.btRating = ratingBuilder(bt);
+
+            /* builds rose data */
+            $('.table-persistent-slab').each(function (i, el) {
+                if(i < 2){
+                    forecast[i] = forecastBuilder($(this))
+                    forecast[i].title = $(this).find('h4').text().replace(/(\s|\n)+/g, "");
+                }
+            });
+
+            report.forecast = forecast;
+        }
+
+        report.save(function (err, doc) {
+            if(err) console.log(err);
+            mongoose.disconnect();  
+        })
+    });
+}
+
+var init = function (mostRecent, callback) {
+    ForecastModel.find().sort('-scrapeDate').skip(mostRecent).exec(function (err, forecasts) {
+        for(var i = 0; i < forecasts.length; i++){
+            forecasts[i].remove();
+        }
+        callback();
+    });
+}
+
 var ratingBuilder = function (rating){
     return rating.toLowerCase().substring(0, rating.length - 4).trim()
 }
 
-/* deletes all entries from db except the 10 most recent */
-cleanHouse(10);
-
-request('http://avalanche.state.co.us/caic/pub_bc_avo.php?zone_id=1', function (error, response, body) {
-
-    if(error) console.log(error);
-
-    if (!error && response.statusCode == 200) {
-        $ = cheerio.load(body);
-
-        var forecast = [];
-
-        var report = new ForecastModel();
-
-        /* forecast date */
-        var forecastDate = $('.caption-controls').first().html();
-        forecastDate = forecastDate.substring(0, forecastDate.indexOf('<span'));
-        report.forecastDate = forecastDate.replace(/(\t|\n)+/g, "").trim();
-
-        /* zone 
-         * hard coded because requires a different url 
-        */
-        var zone = 'front range'
-        report.zone = zone;
-
-        /* danger ratings */
-        var at = $('.above-treeline-image').closest('tr').find('.today-text strong').text();
-        report.atRating = ratingBuilder(at);
-        var tl = $('.near-treeline-image').closest('tr').find('.today-text strong').text();
-        report.tlRating = ratingBuilder(tl);
-        var bt = $('.below-treeline-image').closest('tr').find('.today-text strong').text();
-        report.btRating = ratingBuilder(bt);
-        
-        /* builds rose data */
-        $('.table-persistent-slab').each(function (i, el) {
-            if(i < 2){
-                forecast[i] = forecastBuilder($(this))
-                forecast[i].title = $(this).find('h4').text().replace(/(\s|\n)+/g, "");
-            }
-        });
-
-        report.forecast = forecast;
-    }
-
-    report.save(function (err, doc) {
-        if(err) console.log(err);
-        mongoose.disconnect();  
-    })
-});
+/* cleans db and then gets new report */
+init(10, getReport);
 
