52b34bdc7cb75a4a452c73c28f817a4abfd269be
michael lechner <mlechner2@gmail.com>
2014-03-13 14:03:02 -0600

    Misc development work

--- db-config.js
+++ db-config.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 module.exports = {
     connect: function () {
         if ('production' == process.env.NODE_ENV) {
-            mongoose.connect('mongodb://mlechner2:admin123@oceanic.mongohq.com:10028/app22995659');
+            mongoose.connect(process.env.MONGOHQ_URL);
         }else{
             mongoose.connect('mongodb://localhost/avy-rose');
         }
